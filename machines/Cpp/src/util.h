a1d597019cd38888c80b2abeaec01e4829f768db
Karol Walasek <coreconviction@gmail.com>
2016-10-09 18:55:54 +0200

    Cpp: Applied a third-party itoa() implementation

--- machines/Cpp/src/util.h
+++ machines/Cpp/src/util.h
@@ -1,3 +1,4 @@
+#pragma once
 #define CONCAT(A,B) A B
 #define ROUND_UP(N, S) ((((N) + (S) - 1) / (S)) * (S))
 
@@ -13,4 +14,36 @@
 	#define debug_printf(...) 0
 	#define debug_cout(A) 0
 	#define DEBUG_MODE 0
+#endif
+
+#ifndef _MSC_VER
+	/**
+	 * C++ version 0.4 char* style "itoa":
+	 * Written by Lukás Chmela
+	 * Released under GPLv3.
+
+	 */
+	char* itoa(int value, char* result, int base) {
+		// check that the base if valid
+		if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+		char* ptr = result, *ptr1 = result, tmp_char;
+		int tmp_value;
+
+		do {
+			tmp_value = value;
+			value /= base;
+			*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+		} while ( value );
+
+		// Apply negative sign
+		if (tmp_value < 0) *ptr++ = '-';
+		*ptr-- = '\0';
+		while(ptr1 < ptr) {
+			tmp_char = *ptr;
+			*ptr--= *ptr1;
+			*ptr1++ = tmp_char;
+		}
+		return result;
+	}
 #endif
